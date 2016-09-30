29f1aeb74053514d456748ae5428c202f8156478
Tamizhvendan S <tamizh88@gmail.com>
2016-09-30 21:23:03 +0530

    Fixing typos

--- src/Suave.Azure.Functions/Script.fsx
+++ src/Suave.Azure.Functions/Script.fsx
@@ -2,13 +2,6 @@
 #r "./../../packages/System.Net.Http/lib/net46/System.Net.Http.dll"
 open Suave.Http
 open System.Net
-open FSharp.Reflection
 
-let unionCase<'a> name value = 
-    match FSharpType.GetUnionCases typeof<'a> |> Array.filter (fun case -> case.Name = name) with
-    |[|case|] -> 
-        FSharpValue.MakeUnion(case, value) :?> 'a |> Some
-    |_ -> None
-
-unionCase<HttpCode> "HTTP_201" [||]
-
+let code : HttpStatusCode = LanguagePrimitives.EnumOfValue -1
+(string -1)
