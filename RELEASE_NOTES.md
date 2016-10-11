e02de747f1954f6da06d41e4a512d85689239e9f
Tamizhvendan S <tamizh88@gmail.com>
2016-10-11 17:49:54 +0530

    Bump version to 0.0.2

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,4 @@
+#### 0.0.2
+* Changing the .NET Framework to 4.5
 #### 0.0.1
 * Initial release
--- src/Suave.Azure.Functions/AssemblyInfo.fs
+++ src/Suave.Azure.Functions/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Suave.Azure.Functions")>]
 [<assembly: AssemblyProductAttribute("Suave.Azure.Functions")>]
 [<assembly: AssemblyDescriptionAttribute("Suave + Azure Functions = Awesomeness")>]
-[<assembly: AssemblyVersionAttribute("0.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+[<assembly: AssemblyVersionAttribute("0.0.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Suave.Azure.Functions"
     let [<Literal>] AssemblyProduct = "Suave.Azure.Functions"
     let [<Literal>] AssemblyDescription = "Suave + Azure Functions = Awesomeness"
-    let [<Literal>] AssemblyVersion = "0.0.1"
-    let [<Literal>] AssemblyFileVersion = "0.0.1"
+    let [<Literal>] AssemblyVersion = "0.0.2"
+    let [<Literal>] AssemblyFileVersion = "0.0.2"
